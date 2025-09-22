b51800516633918132339dd0a5263198740128de
Claranbr <clara.nobre@escolar.ifrn.edu.br>
2025-09-22 11:53:48 -0300

    começo

---
+++ trabalho 3b 2025/Assets/Personagem.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Personagem : MonoBehaviour
+{
+      
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
